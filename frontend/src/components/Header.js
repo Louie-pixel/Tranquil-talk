3d0fc1009f601fb4a7b91427292e9b5ebbaf2a11
Louie-pixel <130101289+Louie-pixel@users.noreply.github.com>
2024-06-24 13:23:54 +0300

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,16 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      {/* TRANQUIL-TALK*/}
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/dashboard">Dashboard</a></li>
+          <li><a href="/chatroom">Chat Room</a></li>
+          <li><a href="/profile">Profile</a></li>
+          <li><a href="/login">Login</a></li>
+          <li><a href="/register">Register</a></li>
+        </ul>
+      </nav>
     </header>
   );
 }
